77494e8e03b9759fd3c291d68158e9e53d4bfa9c
Liam Beresford <wlhberesford@gmail.com>
2024-07-20 23:26:59 -0400

    Updated styling

--- src/components/Description/description.js
+++ src/components/Description/description.js
@@ -3,6 +3,7 @@ import Hovercard from '../Hovercard/hovercard'
 import './description.css'
 import Empac from '../../img/empac.jpg'
 import Optumn from '../../img/uhg_optum_logo.jpg'
+import Munch from '../../img/Munch_logo_v1.png'
 
 function Description(){
     return (
@@ -14,34 +15,42 @@ function Description(){
         <div className='Desc-Body'>
             <div className='Desc-Card'>
                 <Hovercard title="Education" 
-                text="I am currently a Computer Science and Economics dual major at 
-                    Rensselaer Polytechnic Institute (RPI) with a concentration in 
-                    Machine Learning/ AI and Data. I am on track to earn my bachlors
-                    degree in December 2025 and my masters in Economics at RPI in 
-                    December 2026." 
+                text="My education path has been driven by my goal to find my 
+                    place in a career as a problem solver. Currently, I am a 
+                    Computer Science and Economics dual major at Rensselaer 
+                    Polytechnic Institute (RPI). I plan to leverage my problem-solving 
+                    and computation from CS with an understanding of data and human 
+                    systems from economics. I believe this intersection of subjects allows 
+                    for large systems of human interaction to be made efficient in a way 
+                    that benefits the people. To do this I have been focusing my classes in 
+                    CS around data, AI, and machine learning, and for Economics, I have been 
+                    focusing my courses on Game Theory and Econometrics. " 
                 pic={Empac}/>
             </div>
             <div className='Desc-Card'>
                 <Hovercard title = "Experience" 
-                text = "This Summer I am an intern with Optum, part of United 
-                    HealthGroup. During this internship I helped start and 
-                    deploy an application leveraging AI to create human like 
-                    conversation for automated calls to health providers. 
-                    During the expeience I built a UI for the application, created 
-                    an AWS database to log call information and used AI for sentiment
-                    analysis and creating call summaries."
+                text = "This summer (2024), I interned with Optum, part of United 
+                        HealthGroup. During this internship, I helped start and 
+                        deploy an application leveraging AI to create human-like 
+                        conversation for automated calls to health providers. My 
+                        main roles in this project have been: creating a MySQL database 
+                        hosted on an AWS RDS server and implementing this server within 
+                        the backend, creating an algorithmic approach to evaluate AI-made
+                         calls to provide a deterministic metric to evaluate calls, and 
+                         leading the front-end development to create a front page for 
+                         the app that follows company brand guidelines."
                 pic={Optumn} />
             </div>
             <div className='Desc-Card'>
                 <Hovercard title = "Projects" 
-                text = "I've started to put mor enphasis on projects made on my own time.
-                        Starting spring 2024 I began a project through Rensselaer Center for
+                text = "I've started to put more emphasis on projects made on my own time.
+                        Starting spring of 2024 I began a project through the Rensselaer Center for
                         Open Source called Munch. This web app is designed to create a better
                         user experience for the dining halls at RPI. A minimal viable product
-                        is currently being made to reduce time to see current options at dining 
+                        is currently being made to reduce the time to see current options at dining 
                         halls from 5 minutes to less than a minute. Future features will add accounts
-                        and rating system that could potenially provide feeback to the dininghalls."
-                pic={Optumn} />
+                        and a rating system that could potentially provide feedback to the dining halls."
+                pic={Munch} />
             </div>
         </div>
         
