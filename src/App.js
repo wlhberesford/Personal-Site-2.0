2ee53765ea74e02c49d39889659b6d965063eaad
Liam Beresford <wlhberesford@gmail.com>
2024-07-31 17:08:16 -0400

    Added Drop down

--- src/App.js
+++ src/App.js
@@ -4,13 +4,9 @@ import Intro from './components/Intro/intro';
 import Description from './components/Description/description';
 import Footer from './components/Footer/footer';
 
-import Favicon from '../src/img/favicon.png'
-import Favicon from "react-favicon";
 
 const App = () => {
-  const [faviconUrl, setFaviconUrl] = useState(
-    Favicon
-        );
+  
   return (
     <div  className="App" >
       <Navbar />
--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,6 +1,49 @@
 import React from 'react'
 import "./navbar.css"
 import headshot from "../../img/HeadShotW24.jpg"
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
+function DropDownMenu(){
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        <img className ='My-Img' src={headshot} alt='OH NO I FORGOT A PITURE HERE' />
+        
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
+}
 
 
 function Navbar() {
@@ -10,9 +53,9 @@ function Navbar() {
             <p className='Nav-Name'>Liam Beresford</p>
         </div>
         <div className='Nav-Right'>
-        <div className='My-Img-Div'>
-                <img className ='My-Img' src={headshot} alt='OH NO I FORGOT A PITURE HERE' />
-            </div>
+          <div className='My-Img-Div'>
+            {DropDownMenu()}
+          </div>
         </div>
 
         
